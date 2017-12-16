5d0674b35a4bd8ebff3afef472c23c64ebc8b510
Dominik Biedebach <6538827+bdbch@users.noreply.github.com>
2017-12-16 03:06:49 +0100

    added auto-redirect and default channel

--- src/Components/ViewServer/index.js
+++ src/Components/ViewServer/index.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+import DemoServer from '../../Helpers/demoServer.json'
 
 export default class ViewServer extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      defaultChannel: DemoServer.defaultChannel
+    }
+  }
+
   render () {
     return (
       <div>
         <h1>Server</h1>
+        <Redirect to={`/server/channels/${this.state.defaultChannel}`} />
       </div>
     )
   }
