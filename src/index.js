604769d73d7de834f9818a429ad0aefb6aa6762f
Dominik Biedebach <6538827+bdbch@users.noreply.github.com>
2017-12-28 00:36:55 +0100

    added Redux to Application

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './Components/App'
+import Store from './Store/configure'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+document.getElementById('root'))
