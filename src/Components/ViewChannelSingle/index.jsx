9cc19c724980dcf18f6e4ad43c98fdc9163d0608
Dominik Biedebach <6538827+bdbch@users.noreply.github.com>
2017-12-28 15:26:08 +0100

    Removed Linting Errors

--- src/Components/ViewChannelSingle/index.jsx
+++ src/Components/ViewChannelSingle/index.jsx
@@ -1,16 +1,10 @@
 import React, { Component } from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 
 import Navbar from '../Navbar/index.jsx'
 import Sidebar from '../Sidebar/index.jsx'
 
-import DemoServer from '../../Helpers/demoServer.json'
-
 class ViewChannelSingle extends Component {
-  constructor (props) {
-    super(props)
-  }
-
   render () {
     return (
       <div className="App-view">
--- src/Components/ViewServerSettings/index.jsx
+++ src/Components/ViewServerSettings/index.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 
-import { Link } from 'react-router-dom'
-
 export default class ViewServerSettings extends Component {
   render () {
     return (
